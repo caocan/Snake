0a9d37e478334977f7e3b6d88b09d61737fb3c58
caocan <wwwcaocan@sina.cn>
2017-11-19 11:25:03 +0800

    Snake1.0版本，创建了可关闭的窗口

---
+++ Snake1.0/src/SnakeClient.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class SnakeClient extends Frame {
+
+    public void lauchFrame(){
+        this.setLocation(400, 300);
+        this.setSize(800, 600);
+
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+        this.setResizable(false);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SnakeClient sc = new SnakeClient();
+        sc.lauchFrame();
+    }
+}
